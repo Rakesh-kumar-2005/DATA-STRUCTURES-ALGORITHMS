b78a5afb822d138b58f32f20d45fd01271e79da8
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2025-03-06 00:20:03 +0530

    Update Best_Time_To_Buy_And_Sell_Stocks_i.java

--- Arrays/Best_Time_To_Buy_And_Sell_Stocks_i.java
+++ Arrays/Best_Time_To_Buy_And_Sell_Stocks_i.java
@@ -1,5 +1,22 @@
 package Arrays;
 
+/*
+        Description :-
+            The code finds the maximum profit from stock prices by keeping track of
+            the minimum price and calculating the maximum profit possible by selling
+            at the current price...
+
+
+        Approach :-
+            > The approach uses a single pass through the prices array.
+            > It initializes min to track the lowest stock price encountered
+                and max_profit to track the highest profit achieved...
+            > For each price in the array, it updates min if the current price is
+                lower than the previous minimum. It then calculates the potential
+                profit by subtracting min from the current price and updates max_profit
+                if the new profit is greater than the previous maximum...
+            > This ensures an efficient solution with O(n) complexity...
+*/
 
 public class Best_Time_To_Buy_And_Sell_Stocks_i {
 
