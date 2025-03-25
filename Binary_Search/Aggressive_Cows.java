db5a388aed6ffd62a0742d0b72426f6462428767
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2025-03-25 22:24:06 +0530

    Update Aggressive_Cows.java

--- Binary_Search/Aggressive_Cows.java
+++ Binary_Search/Aggressive_Cows.java
@@ -37,43 +37,43 @@ import java.util.Arrays;
 
 public class Aggressive_Cows {
 
-	private static boolean canWePlace(int[] stalls, int dist, int cows) {
-		int n = stalls.length;
-		int cntCows = 1;
-		int last = stalls[ 0 ];
-		
-		for (int i = 1; i < n; i++) {
-			if (stalls[ i ] - last >= dist) {
-				cntCows++;
-				last = stalls[ i ];
-			}
-			if (cntCows >= cows) return true;
-		}
-		return false;
-	}
+    private static boolean canWePlace(int[] stalls, int dist, int cows) {
+        int n = stalls.length;
+        int cntCows = 1;
+        int last = stalls[0];
 
-	private static int aggressiveCows(int[] stalls, int k) {
-		int n = stalls.length;
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - last >= dist) {
+                cntCows++;
+                last = stalls[i];
+            }
+            if (cntCows >= cows) return true;
+        }
+        return false;
+    }
 
-		Arrays.sort(stalls);
-		int low = 1;
-		int high = stalls[ n - 1 ] - stalls[ 0 ];
+    private static int aggressiveCows(int[] stalls, int k) {
+        int n = stalls.length;
 
-		while (low <= high) {
-			int mid = (low + high) / 2;
-			if (canWePlace(stalls, mid, k)) {
-				low = mid + 1;
-			} else{
-				high = mid - 1;
-			}
-		}
-		return high;
-	}
+        Arrays.sort(stalls);
+        int low = 1;
+        int high = stalls[n - 1] - stalls[0];
 
-	public static void main(String[] args) {
-		int[] stalls = {0, 3, 4, 7, 10, 9};
-		int k = 4;
-		int ans = aggressiveCows(stalls, k);
-		System.out.println("The maximum possible minimum distance is: " + ans);
-	}
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (canWePlace(stalls, mid, k)) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+
+    public static void main(String[] args) {
+        int[] stalls = {0, 3, 4, 7, 10, 9};
+        int k = 4;
+        int ans = aggressiveCows(stalls, k);
+        System.out.println("The maximum possible minimum distance is: " + ans);
+    }
 }
