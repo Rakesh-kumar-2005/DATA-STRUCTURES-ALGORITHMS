f1309c8378fd11ea8c19e80ac406ab461079206c
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2025-03-26 09:51:50 +0530

    Update Kth_Missing_Positive_Number.java

--- Binary_Search/Kth_Missing_Positive_Number.java
+++ Binary_Search/Kth_Missing_Positive_Number.java
@@ -21,28 +21,28 @@ import java.util.Arrays;
 
 public class Kth_Missing_Positive_Number {
 
-	private static int findKthPositive(int[] arr, int k) {
-		int n = arr.length;
-		int low = 0;
-		int high = n - 1;
+    private static int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
 
-		while (low <= high) {
-			int mid = (low + high) / 2;
-			int missing = arr[ mid ] - (mid + 1);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int missing = arr[mid] - (mid + 1);
 
-			if (missing < k) {
-				low = mid + 1;
-			} else {
-				high = mid - 1;
-			}
-		}
-		return low + k;
-	}
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low + k;
+    }
 
-	public static void main(String[] args) {
-		int[] arr = {2, 3, 4, 7, 11};
-		int k = 5;
-		System.out.println(Arrays.toString(arr));
-		System.out.println("The " + k + "th missing positive number in the above array is = " + findKthPositive(arr, k));
-	}
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 7, 11};
+        int k = 5;
+        System.out.println(Arrays.toString(arr));
+        System.out.println("The " + k + "th missing positive number in the above array is = " + findKthPositive(arr, k));
+    }
 }
