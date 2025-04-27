74fe9f2f16516c56fffef600f1df80f793f6186e
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2025-04-27 18:27:19 +0530

    Add files via upload

---
+++ Tree/Left_View_Of_Binary_Tree.java
@@ -0,0 +1,99 @@
+package Tree;
+
+import java.util.ArrayList;
+
+public class Left_View_Of_Binary_Tree {
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    private static ArrayList<Integer> leftSideView(TreeNode root) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        int currLevel = 0;
+        recursionLeft(root, currLevel, ans);
+
+        return ans;
+    }
+
+    private static void recursionLeft(TreeNode root, int currLevel, ArrayList<Integer> ans) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (currLevel == ans.size()) {
+            ans.add(root.val);
+        }
+
+        recursionLeft(root.left, currLevel + 1, ans);
+        recursionLeft(root.right, currLevel + 1, ans);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+
+        // Level 1
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        // Level 2
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        // Level 3
+        root.left.left.left = new TreeNode(8);
+        root.left.left.right = new TreeNode(9);
+        root.left.right.left = new TreeNode(10);
+        root.left.right.right = new TreeNode(11);
+        root.right.left.left = new TreeNode(12);
+        root.right.left.right = new TreeNode(13);
+        root.right.right.left = new TreeNode(14);
+        root.right.right.right = new TreeNode(15);
+
+        // Level 4
+        root.left.left.left.left = new TreeNode(16);
+        root.left.left.left.right = new TreeNode(17);
+        root.left.left.right.left = new TreeNode(18);
+        root.left.left.right.right = new TreeNode(19);
+        root.left.right.left.left = new TreeNode(20);
+        root.left.right.left.right = new TreeNode(21);
+        root.left.right.right.left = new TreeNode(22);
+        root.left.right.right.right = new TreeNode(23);
+        root.right.left.left.left = new TreeNode(24);
+        root.right.left.left.right = new TreeNode(25);
+        root.right.left.right.left = new TreeNode(26);
+        root.right.left.right.right = new TreeNode(27);
+        root.right.right.left.left = new TreeNode(28);
+        root.right.right.left.right = new TreeNode(29);
+        root.right.right.right.left = new TreeNode(30);
+        root.right.right.right.right = new TreeNode(31);
+
+        ArrayList<Integer> ans = leftSideView(root);
+        System.out.println("Left side view of the Binary Tree is : ");
+
+        for (int val : ans) {
+            System.out.print(val + " ");
+        }
+
+    }
+
+}
