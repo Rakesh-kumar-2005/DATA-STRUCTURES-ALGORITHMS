a7e6f0fe31870761cca845f155d87867fbba393d
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2025-05-10 14:31:50 +0530

    Add files via upload

---
+++ Tree/Serialize_A_Binary_Tree.java
@@ -0,0 +1,95 @@
+package Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Serialize_A_Binary_Tree {
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    private static String serialize(TreeNode root) {
+
+        if (root == null) {
+            return "";
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (! q.isEmpty()) {
+
+            TreeNode currNode = q.poll();
+
+            if (currNode == null) {
+                ans.append("X ");
+                continue;
+            }
+
+            ans.append(currNode.val).append(" ");
+            q.add(currNode.left);
+            q.add(currNode.right);
+        }
+
+        return ans.toString();
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+
+        // Level 1
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        // Level 2
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        // Level 3
+        root.left.left.left = new TreeNode(8);
+        root.left.left.right = new TreeNode(9);
+        root.left.right.left = new TreeNode(10);
+        root.left.right.right = new TreeNode(11);
+
+        root.right.left.right = new TreeNode(13);
+        root.right.right.left = new TreeNode(14);
+        root.right.right.right = new TreeNode(15);
+
+        // Level 4
+        root.left.left.left.left = new TreeNode(16);
+        root.left.left.left.right = new TreeNode(17);
+        root.left.left.right.left = new TreeNode(18);
+        root.left.left.right.right = new TreeNode(19);
+        root.left.right.left.left = new TreeNode(20);
+        root.left.right.left.right = new TreeNode(21);
+        root.left.right.right.left = new TreeNode(22);
+        root.left.right.right.right = new TreeNode(23);
+        root.right.left.right.left = new TreeNode(26);
+        root.right.left.right.right = new TreeNode(27);
+        root.right.right.left.left = new TreeNode(28);
+        root.right.right.left.right = new TreeNode(29);
+        root.right.right.right.left = new TreeNode(30);
+        root.right.right.right.right = new TreeNode(31);
+
+        String serializedTree = serialize(root);
+
+        System.out.println(serializedTree);
+    }
+
+}
