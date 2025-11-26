896efa9893d9f5291374244909e4339e9e4780fe
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2025-11-26 23:39:44 +0530

    Add files via upload

---
+++ Graph/Number_Of_Distinct_Islands_II.java
@@ -0,0 +1,152 @@
+package Graph;
+
+import java.util.ArrayList;
+
+public class Number_Of_Distinct_Islands_II {
+
+    static class Disjoint {
+        ArrayList<Integer> parent = new ArrayList<>();
+        ArrayList<Integer> size = new ArrayList<>();
+
+        public Disjoint(int n) {
+            for (int i = 0; i < n; i++) {
+                parent.add(i);
+                size.add(1);
+            }
+        }
+
+        public int findParent(int node) {
+            if (parent.get(node) == node) {
+                return node;
+            }
+
+            int ultimateParent = findParent(parent.get(node));
+            parent.set(node, ultimateParent);
+            return ultimateParent;
+        }
+
+        public void unionBySize(int u, int v) {
+            int ulp_u = findParent(u);
+            int ulp_v = findParent(v);
+
+            if (ulp_u == ulp_v) {
+                return;
+            }
+
+            if (size.get(ulp_u) > size.get(ulp_v)) {
+                parent.set(ulp_v, ulp_u);
+                size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+            } else {
+                parent.set(ulp_u, ulp_v);
+                size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
+            }
+        }
+    }
+
+    private static boolean isValid(int nRow, int nCol, int n, int m) {
+        return nRow >= 0 && nRow < n && nCol >= 0 && nCol < m;
+    }
+
+    public static ArrayList<Integer> noOfDistinctIslands(int n, int m, int[][] grid) {
+        Disjoint ds = new Disjoint(n * m);
+        int[][] visited = new int[n][m];
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            int row = grid[i][0];
+            int col = grid[i][1];
+
+            if (visited[row][col] == 1) {
+                ans.add(count);
+                continue;
+            }
+
+            visited[row][col] = 1;
+            count++;
+
+            int[] dr = {- 1, 0, 1, 0};
+            int[] dc = {0, 1, 0, - 1};
+
+            for (int j = 0; j < 4; j++) {
+                int nRow = row + dr[j];
+                int nCol = col + dc[j];
+
+                if (isValid(nRow, nCol, n, m) && visited[nRow][nCol] == 1) {
+                    int nodeNo = row * m + col;
+                    int adjNodeNo = nRow * m + nCol;
+
+                    if (ds.findParent(nodeNo) != ds.findParent(adjNodeNo)) {
+                        count--;
+                        ds.unionBySize(nodeNo, adjNodeNo);
+                    }
+                }
+            }
+
+            ans.add(count);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=== Test Case 1: Two Separate Islands ===");
+        int[][] coordinates1 = {
+            {0, 0}, {0, 1}, {0, 2},  // Island 1 (horizontal line)
+            {2, 2}, {2, 3}, {3, 2}   // Island 2 (L-shape)
+        };
+        ArrayList<Integer> result1 = noOfDistinctIslands(4, 4, coordinates1);
+        System.out.println("Coordinates processed: ");
+        for (int i = 0; i < coordinates1.length; i++) {
+            System.out.println("  [" + coordinates1[i][0] + "," + coordinates1[i][1] + "] -> Islands: " + result1.get(i));
+        }
+        System.out.println("Final island count: " + result1.get(result1.size() - 1));
+        System.out.println();
+
+        System.out.println("=== Test Case 2: Single Connected Island (given in random order) ===");
+        int[][] coordinates2 = {
+            {1, 1}, {0, 1}, {1, 0}, {1, 2}, {2, 1}  // All connected in a + shape
+        };
+        ArrayList<Integer> result2 = noOfDistinctIslands(3, 3, coordinates2);
+        System.out.println("Coordinates processed: ");
+        for (int i = 0; i < coordinates2.length; i++) {
+            System.out.println("  [" + coordinates2[i][0] + "," + coordinates2[i][1] + "] -> Islands: " + result2.get(i));
+        }
+        System.out.println("Final island count: " + result2.get(result2.size() - 1));
+        System.out.println();
+
+        System.out.println("=== Test Case 3: Three Islands, One Gets Connected Later ===");
+        int[][] coordinates3 = {
+            {0, 0},           // Island 1
+            {0, 2},           // Island 2
+            {2, 0}, {2, 1},   // Island 3
+            {0, 1}            // Connects Island 1 and 2!
+        };
+
+        ArrayList<Integer> result3 = noOfDistinctIslands(3, 3, coordinates3);
+        System.out.println("Coordinates processed: ");
+
+        for (int i = 0; i < coordinates3.length; i++) {
+            System.out.println("  [" + coordinates3[i][0] + "," + coordinates3[i][1] + "] -> Islands: " + result3.get(i));
+        }
+
+        System.out.println("Final island count: " + result3.get(result3.size() - 1));
+        System.out.println("\n=== Test Case 4: Duplicate Coordinates ===");
+
+        int[][] coordinates4 = {
+            {0, 0}, {0, 1}, {0, 0}, {1, 1}  // {0,0} appears twice
+        };
+
+        ArrayList<Integer> result4 = noOfDistinctIslands(2, 2, coordinates4);
+        System.out.println("Coordinates processed: ");
+
+        for (int i = 0; i < coordinates4.length; i++) {
+            System.out.println("  [" + coordinates4[i][0] + "," + coordinates4[i][1] + "] -> Islands: " + result4.get(i));
+        }
+
+        System.out.println("Final island count: " + result4.get(result4.size() - 1));
+
+    }
+
+}
