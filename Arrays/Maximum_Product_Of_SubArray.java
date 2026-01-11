b8adf9e44eed622b2873c4bf3090d5a29f0e01b7
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2026-01-11 19:12:31 +0530

    Update Maximum_Product_Of_SubArray.java

--- Arrays/Maximum_Product_Of_SubArray.java
+++ Arrays/Maximum_Product_Of_SubArray.java
@@ -1,6 +1,7 @@
 package Arrays;
 
 /*
+
         Description :-
             We have to find the maximum product of any contiguous sub-array within a given array,
             with optimized space and time complexity...
@@ -14,13 +15,17 @@ package Arrays;
                     numbers properly.
             > Time Complexity = O(n) because of a single traversal...
             > Space Complexity = O(1) because we didn't use any extra-space to solve the question...
+            
 */
 
 public class Maximum_Product_Of_SubArray {
 
     private static int product(int[] arr) {
+            
         int n = arr.length;
-        if (n == 0) return -1;
+        if (n == 0) {
+            return -1;
+        }       
 
         int prefix = 1;
         int suffix = 1;
@@ -39,7 +44,10 @@ public class Maximum_Product_Of_SubArray {
     }
 
     public static void main(String[] args) {
+            
         int[] arr = {1, 2, 3, 0, 4, 2};
         System.out.println(product(arr));
+    
     }
+        
 }
