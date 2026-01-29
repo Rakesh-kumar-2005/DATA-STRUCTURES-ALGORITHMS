933b5cf7b057820f68da2362f8332724ad866163
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2026-01-29 09:25:11 +0530

    Update Rat_Maze.java

--- BackTracking/Rat_Maze.java
+++ BackTracking/Rat_Maze.java
@@ -19,18 +19,26 @@ package BackTracking;
 
 
 public class Rat_Maze {
+        
     private static void printWays(int sr, int sc, int er, int ec, String s) {
+            
         if (sr > er || sc > ec) return;
         if (sr == er && sc == ec) {
             System.out.println(s);
             return;
         }
+            
         printWays(sr + 1, sc, er, ec, s + "D");
         printWays(sr, sc + 1, er, ec, s + "R");
+            
     }
+        
     public static void main(String[] args) {
+            
         int rows = 3;
         int cols = 3;
         printWays(0,0,rows-1,cols-1,"");
+            
     }
+        
 }
