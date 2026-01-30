fb235ca7b8a8e105d5fa0ef2db662dccd06b38f5
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2026-01-30 19:46:42 +0530

    Update Rat_Maze_With_Bockage.java

--- BackTracking/Rat_Maze_With_Bockage.java
+++ BackTracking/Rat_Maze_With_Bockage.java
@@ -30,7 +30,9 @@ package BackTracking;
 
 
 public class Rat_Maze_With_Bockage {
+    
     public static void printWays(int sr, int sc, int er, int ec, String s, boolean[][] isVisited){
+        
         if(sr < 0 || sc < 0) return;
         if(sr > er || sc > ec) return;
         if(isVisited[sr][sc]) return;
@@ -38,17 +40,23 @@ public class Rat_Maze_With_Bockage {
             System.out.println(s);
             return;
         }
+        
         isVisited[sr][sc] = true;
         printWays(sr+1, sc, er, ec, s+'D', isVisited);
         printWays(sr, sc+1, er, ec, s+'R', isVisited);
         printWays(sr, sc-1, er, ec, s+'L', isVisited);
         printWays(sr-1, sc, er, ec, s+'U', isVisited);
         isVisited[sr][sc] = false;
-}
+    }
+    
     public static void main(String[] args) {
+            
         int rows = 3;
         int cols = 3;
+        
         boolean[][] isVisited = new boolean[rows][cols];
         printWays(0,0,rows-1,cols-1,"",isVisited);
+        
     }
+    
 }
